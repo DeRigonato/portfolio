7eec1667eac3856b7f298fa56eeb80dd0817bc60
André Gotardo <al.g.regonato@gmail.com>
2025-07-01 20:45:07 -0300

    Alteração simples

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,9 +3,9 @@ export const projects = [
     id: 1,
     title: "Whatsapp Weather",
     description: "Aplicação em  Node.js que consome API de clima e avisa caso chova por Whatsapp via CallmeBot ",
-    stack: ["Node.Js", "React", "PostgreSQL"],
-    challenge: "Otimização de queries em 300% usando índices",
-    githubUrl: "#"
+    stack: ["Node.Js", "React", "RestAPI"],
+    challenge: "Estabelecer comunicação entre duas API's ",
+    githubUrl: "https://github.com/DeRigonato/whatsapp-weather"
   },
   // Adicione outros projetos
 ];
